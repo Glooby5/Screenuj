ea3f36a82df806affc0b5f5dbc01ac1f140a185b
Jan Kaderabek <kaderabek.jan@gmail.com>
2015-07-20 16:00:35 +0200

    work fb login

--- app/presenters/SignPresenter.php
+++ app/presenters/SignPresenter.php
@@ -2,36 +2,59 @@
 
 namespace App\Presenters;
 
-use Nette,
-	App\Forms\SignFormFactory;
-
+use Screenuj\Services\UserService;
+use Screenuj\Entities;
 
 /**
- * Sign in/out presenters.
+ * Presenter for log in from Facebook, Google, ...
  */
 class SignPresenter extends BasePresenter
 {
-
-
-	/**
-	 * Sign-in form factory.
-	 * @return Nette\Application\UI\Form
-	 */
-	protected function createComponentSignInForm()
-	{
-		$form = $this->factory->create();
-		$form->onSuccess[] = function ($form) {
-			$form->getPresenter()->redirect('Homepage:');
-		};
-		return $form;
-	}
-
-
-	public function actionOut()
-	{
-		$this->getUser()->logout();
-		$this->flashMessage('You have been signed out.');
-		$this->redirect('in');
-	}
-
+    /** @inject @var UserService */
+    public $userService;
+    
+    public function __construct(UserService $userService)
+    {
+        parent::__construct();
+        $this->userService = $userService;
+    }
+    
+    public function handleFacebook($fbId, $name, $email, $token)
+    {
+        //@todo: přidání do databáze a vytvoření identity
+        $user = $this->userService->findOneBy(['facebook' => $fbId]);
+        
+        if (!$user) //Neexistuje uživatel se zadaným FB ID
+        {
+            $user = $this->userService->findOneBy(['email' => $email]);
+            
+            if (!$user) //Neexistuje uživatel se stejným emailem
+            {
+                $user = new Entities\User($name, $email, $fbId);
+                $this->userService->save($user);    
+                
+            } 
+            else 
+            {
+                /** @todo Sloučit účty */
+            }
+        }
+        $data = ['email' => $user->email, 'name' => $user->name, 'type' => 'Facebook', 'signed' => new \DateTime()];          
+        $identity = new \Nette\Security\Identity($user->id, NULL, $data);
+        
+        $this->flashMessage('You have been signed in.');
+        $this->user->login($identity);
+        $this->template->user = $this->user;
+        $this->redrawControl("loginSnippet");
+    }
+    
+    public function handleOut()
+    {
+        $this->getUser()->logout();
+        $this->flashMessage('You have been signed out.');
+        
+        $this->template->user = $this->user;
+        $this->redrawControl("loginSnippet");
+    }
+    
 }
--- www/js/fb.js
+++ www/js/fb.js
@@ -1,26 +1,3 @@
-function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
-
-    if (response.status === 'connected') {
-        // Logged into your app and Facebook.
-        testAPI();
-    } else if (response.status === 'not_authorized') {
-        // The person is logged into Facebook, but not your app.
-        document.getElementById('status').innerHTML = 'Please log into this app.';
-    } else {
-        // The person is not logged into Facebook, so we're not sure if they are logged into this app or not.
-        document.getElementById('status').innerHTML = 'Please log into Facebook.';
-    }
-}
-
-//called when someone finishes with the Login Button.
-function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-        statusChangeCallback(response);
-    });
-}
-
 window.fbAsyncInit = function() {
     FB.init({
         appId      : '1480642292229101',
@@ -29,13 +6,6 @@ window.fbAsyncInit = function() {
         xfbml      : true,
         version    : 'v2.4'
     });
-
-    // 1. Logged into your app ('connected')
-    // 2. Logged into Facebook, but not your app ('not_authorized')
-    // 3. Not logged into Facebook and can't tell if they are logged into
-    FB.getLoginStatus(function(response) {
-        statusChangeCallback(response);
-    });
 };
 
 // Load the SDK asynchronously
@@ -43,22 +13,21 @@ window.fbAsyncInit = function() {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk/debug.js";
-    //js.src = "//connect.facebook.net/en_US/sdk.js";
+    js.src = "//connect.facebook.net/cs_CZ/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
 } (document, 'script', 'facebook-jssdk'));
 
-function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', {fields: 'id, name, email'}, function(response) {
-        console.log(response);
-        console.log('Successful login for: ' + response.name);
-        document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
-        FbLogin(response.id, response.name, response.email);
-    });
-}
-
+$(document).on('click', '#fb-login', function (event) {
+    FB.login(function(response) {
+        if (response.authResponse) {
+            FB.api('/me', {fields: 'id, name, email'}, function(response) {
+                FbLogin(response.id, response.name, response.email);
+            });
+        } else {
+            console.log('Authorization failed.');
+        }
+    }, {scope: 'email'});
+});
 
 function FbLogin(id, name, email) {
     $.nette.ajax({
